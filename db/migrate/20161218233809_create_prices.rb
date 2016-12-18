3d20cbaef5e5a94d164f6f5c22fcc842751badfe
zitongliu <zitongliu7@gmail.com>
2016-12-19 10:53:20 +1100

    Generated database schema

--- db/migrate/20161218233809_create_prices.rb
+++ db/migrate/20161218233809_create_prices.rb
@@ -2,6 +2,12 @@ class CreatePrices < ActiveRecord::Migration
   def change
     create_table :prices do |t|
 
+      t.text :month
+      t.text :year
+      t.integer :mean_b2
+      t.integer :mean_b3
+      t.integer :mean_b4
+      t.integer :suburb_id # Foreign key
       t.timestamps null: false
     end
   end
--- db/migrate/20161218233854_create_suburbs.rb
+++ db/migrate/20161218233854_create_suburbs.rb
@@ -2,6 +2,10 @@ class CreateSuburbs < ActiveRecord::Migration
   def change
     create_table :suburbs do |t|
 
+      t.text :name
+      t.integer :postcode
+      t.float :latitude
+      t.float :longitude
       t.timestamps null: false
     end
   end
