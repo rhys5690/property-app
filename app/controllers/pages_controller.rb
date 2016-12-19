a6f0a6d8e7ad2d7113c31f36b9d84ca36d59a81a
rhys5690 <rhys5690@gmail.com>
2016-12-19 15:58:02 +1100

    added form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def search
+    @suburb = Suburb.where({:name => params[:suburb]}).first
+    @bedrooms = params[:bedrooms]
+    @prices = @suburb.prices[0].mean_b3
+    User.create({:email => params[:email]})
+    # Suburb.where({:name => params[:suburb]})
+    # @prices = @suburb.prices
+  end
+
 end
