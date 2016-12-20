e442fdb8c4a19cec838051dcd0de11994f572467
zitongliu <zitongliu7@gmail.com>
2016-12-20 12:24:21 +1100

    Added average price per suburb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,23 +2,8 @@ class PagesController < ApplicationController
 
   def home
 
-    # @all_suburbs = Suburb.all
-    # number_of_prices = @all_suburbs.first.prices.count
-    # total_of_prices = 0
-    # @suburb_average_array = Array.new
-    #
-    # @all_suburbs.each do |suburb|
-    #   suburb_total = 0
-    #
-    #   suburb.prices.each do |price|
-    #     total_of_prices = total_of_prices + price.mean_b3
-    #     suburb_total += price.mean_b3
-    #   end
-    #
-    #   @suburb_average_array.append( suburb_total/number_of_prices )
-    # end
-    #
-    # @average_all_suburbs = total_of_prices / number_of_prices
+
+    # The following code trys to find the total price for all suburbs
 
     all_prices = Price.all.pluck(:mean_b3)
     total_price = all_prices.reduce(0) do |total, current_price|
@@ -28,15 +13,37 @@ class PagesController < ApplicationController
 
 
 
-    #  The following try to list all suburbs and their respective average price
+    #  The following code will generate an array containing the name and price for every suburb
+    # In the form of array inside an array [ [suburb_name, suburb_price], [] ]
 
     @all_suburbs = Suburb.all
+    @suburb_price_array = Array.new
+    @suburb_price_history_array = Array.new
 
-    
+    @all_suburbs.each do |suburb|
+      latest_price = suburb.prices[0].mean_b3
+      current_suburb_price_array = [suburb, latest_price]
+      @suburb_price_array.push(current_suburb_price_array)
+
+      # interate through all prices in each suburb
+      current_suburb_price_history_array = Array.new
+      current_suburb_price_history = suburb.prices
+      current_suburb_price_history.each do |price|
+        monthly_price = price.mean_b2
+        month_name = price.suburb.name
+        month_name_and_price = [month_name, monthly_price]
+        current_suburb_price_history_array.push(month_name_and_price)
+      end
+      @suburb_price_history_array.push(current_suburb_price_history_array)
+
+    end
+
+
+    @projected_growth_percentage = @average_all_suburbs
 
 
 
-    # binding.pry
+
 
   end
 
