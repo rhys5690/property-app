33e7dd9cdb2e9a529bda787926c2edb031156a35
rhys5690 <rhys5690@gmail.com>
2016-12-21 15:07:27 +1100

    fixed display

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -159,7 +159,7 @@ $(document).ready(function() {
         // var postcode = data.suburb.postcode;
 
         var $enterEmail = $(".enter-email");
-        $enterEmail.removeClass("none");
+        $enterEmail.addClass("none");
 
         $(".global-stats").addClass("none");
 
@@ -168,40 +168,9 @@ $(document).ready(function() {
         // $displayResult.append("<p>" + postcode + "</p>");
 
         //Begin implementing Bar chart
-
-        var dataTest = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [{
-                label: "My First dataset",
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1,
-                data: [65, 59, 80, 81, 56, 55, 40],
-            }]
-        };
-        var myBarChart = new Chart(ctx, {
-            type: 'bar',
-            data: dataTest,
-            options: options
-        });
-
         var $displayPropertyValue = $(".local-stats");
         $displayPropertyValue.removeClass("none");
-        $displayPropertyValue.append(dataTest);
+
 
 
 
